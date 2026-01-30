e7c3f709e91a1df0597ef30e618b2819638bceb3
kkuoii007-coder <kkuoii007@gmail.com>
2026-01-30 12:18:04 +0300

    Update store.py

---
+++ store.py
@@ -0,0 +1,55 @@
+class Store:
+    def __init__(self, name, adress, items):
+        self.name = name
+        self.adress = adress
+        self.items = items  # items — это словарь: { "товар": цена }
+
+
+store1 = Store("Матрешка", "г. Москва, ул. Фестивальная, д. 14", {"вишня": 5, "бананы": 10})
+store2 = Store("Алена", "г. Москва, ул. Смольная, д. 15", {"газировка": 12, "шоколад": 15})
+
+
+def add_items(name, adress, new_items):
+    if name == store1.name and adress == store1.adress:
+        store = store1
+    else:
+        store = store2
+    store.items.update(new_items)
+
+def remove_item(name, adress, key):
+    if name == store1.name and adress == store1.adress:
+        store = store1
+    else:
+        store = store2
+    removed_value = store.items.pop(key, None)
+
+def up_items(name, adress, new_items):
+    if name == store1.name and adress == store1.adress:
+        store = store1
+    else:
+        store = store2
+    store.items.update(new_items)
+
+def get_item(name, adress, key):
+    if name == store1.name and adress == store1.adress:
+        store = store1
+    else:
+        store = store2
+    return store.items.get(key, None)
+
+
+
+add_items("Матрешка", "г. Москва, ул. Фестивальная, д. 14", {"творог": 6, "хлеб": 8})
+print(store1.items)
+
+ok = remove_item("Матрешка", "г. Москва, ул. Фестивальная, д. 14", "бананы")
+print(store1.items)
+
+add_items("Матрешка", "г. Москва, ул. Фестивальная, д. 14", {"творог": 50})
+print(store1.items)
+
+price = get_item("Матрешка", "г. Москва, ул. Фестивальная, д. 14", "вишня")
+print(price)
+
+price = get_item("Матрешка", "г. Москва, ул. Фестивальная, д. 14", "бананы")
+print(price)
